7e282ba6230f4f9fa11a1e4c7a43c7d6869a389c
MatteoA645 <mralemany@willamette.edu>
2024-10-27 23:14:34 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,11 +1,39 @@
 ##################################################
-# Name:
+# Name: Matteo
 # Collaborators:
-# Estimate of time spent (hr):
+# Estimate of time spent (hr): 
 ##################################################
 
 def is_magic_square(array:list[list[int]]) -> bool:
-    pass
+    
+    sumlist = []
+    rowlist = [sum(i) for i in array]
+    sumlist += rowlist
+    collist = [0] * len(array)
+    diag1=0
+   
+    for i in array:
+        if len(i) != len(array):
+            return False
+        for j in range(len(i)):
+            collist[j] += i[j]
+            if j == array.index(i):
+                diag1 += i[j]
+    
+    diag2 = [array[len(array)-1-i][i] for i in range(len(array)-1,-1,-1)]          
+    sumlist += collist
+    sumlist.append(diag1)
+    sumlist.append(sum(diag2))
+
+    for k in sumlist:
+        if k != 15:
+            return False
+    
+    return True
+    
+
+        
+            
 
 small = [[8,1,6],[3,5,7],[4,9,2]]
 print(is_magic_square(small))
